1640f23f8abc5c294c1e7f64de686045b72368a6
Likith Gowda S <105909257+likithgowda18@users.noreply.github.com>
2024-10-10 16:46:56 +1100

    commit
    commit

--- 6.2P-resources/project_part2/book_catalog/config.py
+++ 6.2P-resources/project_part2/book_catalog/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    DATABASE_URL: str = os.getenv("postgresql://bookdb_9v1l_user:vV8FHZWV8peiImLkZzYRFxE782jkDoI1@dpg-cs3mhlggph6c73c4dltg-a.oregon-postgres.render.com/bookdb_9v1l")
 
 settings = Settings()
